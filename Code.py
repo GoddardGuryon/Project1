9db444f24df7805e6abbdc099a2ce77d36aa3fdb
Vihangbodh <9369053+Vihangbodh@users.noreply.github.com>
2019-04-14 16:57:09 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,4 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-census_df = pd.DataFrame
+census_df = pd.DataFrame('India_Census_Data.xlsx')
+religion_df = pd.DataFrame('RL-0000.xlsx')
+
